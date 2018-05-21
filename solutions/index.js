9aad9556a681305b4dc55ea94fb5099dd661ba80
oliverjam <oliverjamesphillips@gmail.com>
2018-05-21 22:49:56 +0100

    Add solution

---
+++ solutions/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+// sumArray takes an array of two numbers and returns the sum of the numbers
+// edit the parameters of the function to make this work
+const sumArray = ([first, second]) => first + second;
+
+// getAttribution takes a post object { title: string, author: string, timestamp: string }
+// edit the parameters to make this work
+const getAttribution = ({ title, author, timestamp }) =>
+  `${title} by ${author} @ ${timestamp}`;
+
+// calculateTotal receives a bill object { subtotal: number, tax: number, tip: number || undefined }
+// the tip might be undefined, in which case it should default to 0.1 (10%)
+const calculateTotal = ({ subtotal, tax, tip = 0.1 }) => {
+  return subtotal * (1 + tax) * (1 + tip);
+};
+
+// ProfileCard receives a data object that contains a user { data: { user: { avatarSrc: string, username: string, githubUrl: string  } } }
+const ProfileCard = ({
+  data: {
+    user: { avatarSrc, username, githubUrl },
+  },
+}) => (
+  <div>
+    <img src={avatarSrc} />
+    <h3>{username}</h3>
+    <a href={githubUrl}>Github profile</a>
+  </div>
+);
+
+// Counter receives a prop called `step` that determines how much to increment each time
+// Edit the render method so that it works properly
+class Counter extends React.Component {
+  state = {
+    count: 0,
+  };
+  render() {
+    const { step } = this.props;
+    const { count } = this.state;
+    return (
+      <button
+        onClick={() =>
+          this.setState(prevState => ({ count: prevState.count + step }))
+        }
+      >
+        Count: {this.state.count}
+      </button>
+    );
+  }
+}
+
+export { sumArray, getAttribution, calculateTotal, ProfileCard, Counter };
