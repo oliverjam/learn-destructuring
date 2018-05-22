4fa14ea2d06627ed1616f63dea97b70410c5781e
zooeymiller <zooeyxmiller@gmail.com>
2018-05-22 08:11:08 +0100

    make count more sensible and counter a little harder

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // sumArray takes an array of two numbers and returns the sum of the numbers
 // edit the parameters of the function to make this work
@@ -11,7 +11,7 @@ const getAttribution = postObject => `${title} by ${author} @ ${timestamp}`;
 // calculateTotal receives a bill object { subtotal: number, tax: number, tip: number || undefined }
 // the tip might be undefined, in which case it should default to 10%
 const calculateTotal = bill => {
-  return subtotal * (1 + tax) * tip;
+  return subtotal * (1 + tax) * (1 + tip);
 };
 
 // ProfileCard receives a data object that contains a user { data: { user: { avatarSrc: string, username: string, githubUrl: string  } } }
@@ -25,15 +25,16 @@ const ProfileCard = data => (
 
 // Counter receives a prop called `step` that determines how much to increment each time
 // Edit the render method so that it works properly
+// You'll also need to look at the onClick on the button
 class Counter extends React.Component {
   state = {
-    count: 0,
+    count: 0
   };
   render() {
     return (
       <button
         onClick={() =>
-          this.setState(prevState => ({ count: prevState + step }))
+          this.setState(prevState => ({ count: prevCount + step }))
         }
       >
         Count: {count}
--- index.test.js
+++ index.test.js
@@ -1,52 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-dom/test-utils';
+import React from "react";
+import ReactDOM from "react-dom";
+import TestUtils from "react-dom/test-utils";
 import {
   sumArray,
   getAttribution,
   calculateTotal,
   ProfileCard,
-  Counter,
-} from './';
+  Counter
+} from "./";
 
-test('sumArray', () => {
+test("sumArray", () => {
   const testArray = [1, 2];
   expect(sumArray(testArray)).toEqual(3);
 });
 
-test('getAttribution', () => {
+test("getAttribution", () => {
   const post = {
-    title: 'Hummus is great',
-    author: 'Zooey',
-    timestamp: '20/05/18',
+    title: "Hummus is great",
+    author: "Zooey",
+    timestamp: "20/05/18"
   };
-  expect(getAttribution(post)).toEqual('Hummus is great by Zooey @ 20/05/18');
+  expect(getAttribution(post)).toEqual("Hummus is great by Zooey @ 20/05/18");
 });
 
-test('calculateTotal', () => {
+test("calculateTotal", () => {
   const bill = { subtotal: 100, tax: 0.2 };
   expect(calculateTotal(bill)).toEqual(132);
 });
 
-test('ProfileCard', () => {
+test("ProfileCard", () => {
   const data = {
     user: {
-      username: 'oliverjam',
-      avatarSrc: 'https://github.com/oliverjam.png',
-      githubUrl: 'https://github.com/oliverjam',
-    },
+      username: "oliverjam",
+      avatarSrc: "https://github.com/oliverjam.png",
+      githubUrl: "https://github.com/oliverjam"
+    }
   };
-  const root = document.createElement('div');
+  const root = document.createElement("div");
   ReactDOM.render(<ProfileCard data={data} />, root);
-  const image = root.querySelector('img');
-  expect(image.src).toEqual('https://github.com/oliverjam.png');
+  const image = root.querySelector("img");
+  expect(image.src).toEqual("https://github.com/oliverjam.png");
 });
 
-test('Counter', () => {
-  const root = document.createElement('div');
+test("Counter", () => {
+  const root = document.createElement("div");
   ReactDOM.render(<Counter step={2} />, root);
-  const button = root.querySelector('button');
-  expect(button.textContent).toEqual('Count: 0');
+  const button = root.querySelector("button");
+  expect(button.textContent).toEqual("Count: 0");
   TestUtils.Simulate.click(button);
-  expect(button.textContent).toEqual('Count: 2');
+  expect(button.textContent).toEqual("Count: 2");
 });
